5b425132e8b7821152f970a6fc4103e71acbff1c
Pedro Ivan Chaves Oliveira <pedroivanchaves@hotmail.com>
2020-11-12 20:45:18 -0300

    Deleted User class

--- app/src/main/java/com/example/splashactivity/UserData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.splashactivity
-
-import android.text.Editable
-import android.widget.EditText
-
-class UserData(val name: Editable?, val email: Editable?, val password: Editable?, val confirmPassword: Editable?)
